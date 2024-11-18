ac805245e544e704f9012ab4893920738ceb15f6
Raryy00 <160739029+Raryy00@users.noreply.github.com>
2024-11-18 11:23:25 -0300

    Add files via upload

--- Atividade 06/script.js
+++ Atividade 06/script.js
@@ -1,3 +1,4 @@
+// Função para contar números de 1 a n
 function contarNumeros() {
     let n = prompt("Digite um número:");
     n = parseInt(n);
@@ -12,4 +13,31 @@ function contarNumeros() {
     for (let i = 1; i <= n; i++) {
         div.innerHTML += i + "<br>";
     }
+}
+
+// Função para calcular as raízes quadradas de 25, 36 e 49
+function calcularRaizes() {
+    const numeros = [25, 36, 49];
+    const raizes = numeros.map(num => Math.sqrt(num));
+    
+    let div = document.getElementById("raizes");
+    div.innerHTML = ""; // Limpa o conteúdo anterior
+
+    raizes.forEach((raiz, index) => {
+        div.innerHTML += `A raiz quadrada de ${numeros[index]} é ${raiz}<br>`;
+    });
+}
+
+// Função para escrever um texto fixo em uma div
+function escreverNaDiv() {
+    const texto = "Olá, este texto foi inserido via JavaScript!";
+    const div = document.getElementById("minhaDiv");
+    div.innerHTML = texto;
+}
+
+// Função para exibir o texto digitado pelo usuário
+function exibirTexto() {
+    const texto = document.getElementById("textoUsuario").value;
+    const div = document.getElementById("resultado");
+    div.innerHTML = texto;
 }

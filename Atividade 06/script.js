79adc83911d1648280a054713fa56ce3b38baa45
Raryy00 <160739029+Raryy00@users.noreply.github.com>
2025-11-17 16:03:53 -0300

    Update script.js

--- Atividade 06/script.js
+++ Atividade 06/script.js
@@ -1,14 +1,14 @@
 function calcularRaiz(numero) {
-    const raiz = Math.sqrt(numero);
-    document.getElementById('resultado').innerText = `A raiz quadrada de ${numero} é ${raiz}`;
+  const raiz = Math.sqrt(numero);
+  document.getElementById('resultado').innerText = `A raiz quadrada de ${numero} é ${raiz}`;
 }
 
 function escreverNaDiv() {
-    const div = document.getElementById('minhaDiv');
-    div.innerText = 'Escrevendo em um elemento div';
+  const div = document.getElementById('minhaDiv');
+  div.innerText = 'Escrevendo em um elemento div';
 }
 
 function receberTexto() {
-    const texto = document.getElementById('textoUsuario').value;
-    document.getElementById('textoResultado').innerText = ` ${texto}`;
+  const texto = document.getElementById('textoUsuario').value;
+  document.getElementById('textoResultado').innerText = `Você digitou: ${texto}`;
 }
